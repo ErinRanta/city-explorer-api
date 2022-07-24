ab06d8a16ab02d5cbf58df29b6e63356c3ddbe76
Erin Ranta <erinran99@gmail.com>
2022-07-24 15:46:58 -0400

    finally got that array

--- server.js
+++ server.js
@@ -1,95 +1,62 @@
 'use strict';
- 
+
 require('dotenv').config();
-// require is a function built into Node, loads exported values into our current file.
-const express = require('express'); // whats happening at this point.
+const express = require('express');
 const cors = require('cors');
-const weatherData = require ('./data/weather.json');
-const server = express();
+const weatherData = require('./data/weather.json');
+const server = express(); 
+const PORT = process.env.PORT || 3001;
 server.use(cors());
- 
- 
- 
- 
-const PORT = process.env.PORT || 3002;
- 
- 
+
 class Forecast {
- constructor(date, description) {
-   this.date = date;
-   this.description = description;
-//    this.description = 'low of ' + obj.low_temp + ', high of ' + obj.high_temp + ' with ' + obj.weather.description.toLowerCase();
- }
+  constructor(obj) {
+    this.date = obj.datetime;
+    this.description = 'low of ' + obj.low_temp + ', high of ' + obj.high_temp + ' with ' + obj.weather.description.toLowerCase();
+  }
 }
- 
- 
-server.get('/', (request, response) => {
- response.status(200).send('go to PORT 3002');
-});
- 
- 
- 
-// create a weather route //
- 
+
+server.use(cors()); // cross origin resource sharing
+
+// create a weather route
 server.get('/weather', (request, response) => {
- console.log(request.query);
- let { lat, lon, searchQuery } = request.query;
- 
- 
+  console.log(request.query);
+  let { lat, lon, searchQuery } = request.query;
+
+  if (!lat || !lon || !searchQuery) {
+    throw new Error('Please send lat lon and search query as a query string');
+  }
+
+  // find appropriate value from weatherData
+  // use search Query to find an object within weather data
 
 
- if (!lat || !lon || !searchQuery) {
-   throw new Error('Please send lat lon and search query as a string');
- }
- 
- 
- 
- // / find appropriate value from weatherData
- // use search Query to find an object within weather data
- 
- let city = weatherData.find(city => {
-   return city.city_name.toLowerCase() === searchQuery.toLowerCase();
- });
+  let city = weatherData.find(city => {
+    return city.city_name.toLowerCase() === searchQuery.toLowerCase();
+  });
 
- if (city) {
-   let forecastArray = city.data.map(forecast => new Forecast(forecast));
-   response.send(forecastArray);
- } else {
-   response.status(404).send('City not found');
- }
+  if (city) {
+    let forecastArray = city.data.map(forecast => new Forecast(forecast));
+    response.send(forecastArray);
+  } else {
+    response.status(404).send('City not found');
+  }
 });
- 
-// function forecastArray(weatherCity) {
-//  const forecastArr = weatherCity.data.map(el => {
-//    let date = el.valid_date;
-//    let condition = el.weather.description;
-//    return new Forecast(date, condition);
-//  });
-//  return forecastArr;
-// }
-//  console.log(forecastArr);
 
- 
- 
- 
-//error section //
- 
-server.get('/error', (request, response) => {
- 
- throw new Error('Error! Something is not working!');
- 
-});
- 
+// error handling//
+
 server.use('*', (error, request, response, next) => {
- response.status(500).send(error);
+  response.status(500).send(error);
 });
- 
+
 server.use('*', (request, response) => {
- console.log('catch all route hit');
- response.status(404).send('Route Not found :(');
+  response.status(404).send('Route not found');
 });
- 
-//server opens up to listen//
+
+//server opens up to  listen//
+
 server.listen(PORT, () => {
- console.log('Server is running on port :: ' + PORT);
+  console.log('Server is running on port : ' + PORT);
 });
+
+
+
