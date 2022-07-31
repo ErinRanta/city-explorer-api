6ed38b943dee3462d4a569dd0553daa8ec307abd
Erin Ranta <erinran99@gmail.com>
2022-07-31 01:52:35 -0400

    lab8

--- server.js
+++ server.js
@@ -1,62 +1,101 @@
 'use strict';
-
+ 
 require('dotenv').config();
+const axios = require('axios');
 const express = require('express');
 const cors = require('cors');
-const weatherData = require('./data/weather.json');
-const server = express(); 
-const PORT = process.env.PORT || 3001;
-server.use(cors());
-
+const app = express();
+const PORT = process.env.PORT;
+ 
+ 
+app.use(cors());
+ 
 class Forecast {
-  constructor(obj) {
-    this.date = obj.datetime;
-    this.description = 'low of ' + obj.low_temp + ', high of ' + obj.high_temp + ' with ' + obj.weather.description.toLowerCase();
-  }
+ constructor(date, condition, high, low) {
+   this.date = date;
+   this.condition = condition;
+   this.high = high;
+   this.low = low;
+   this.weatherAPIKey = process.env.REACT_APP_WEATHERBIT_API_KEY;
+   this.movieAPIKey = process.env.REACT_APP_TMDB_API_KEY;
+ }
 }
-
-server.use(cors()); // cross origin resource sharing
-
-// create a weather route
-server.get('/weather', (request, response) => {
-  console.log(request.query);
-  let { lat, lon, searchQuery } = request.query;
-
-  if (!lat || !lon || !searchQuery) {
-    throw new Error('Please send lat lon and search query as a query string');
-  }
-
-  // find appropriate value from weatherData
-  // use search Query to find an object within weather data
-
-
-  let city = weatherData.find(city => {
-    return city.city_name.toLowerCase() === searchQuery.toLowerCase();
-  });
-
-  if (city) {
-    let forecastArray = city.data.map(forecast => new Forecast(forecast));
-    response.send(forecastArray);
-  } else {
-    response.status(404).send('City not found');
-  }
+ 
+function makeForecastArray(weatherCity) {
+ const forecastArr = weatherCity.map(el => {
+   let date = el.valid_date;
+   let condition = el.weather.description;
+   let high = el.high_temp;
+   let low = el.low_temp;
+   return new Forecast(date, condition, high, low);
+ });
+ return forecastArr;
+}
+ 
+class Movie {
+ constructor(title, overview, releaseDate, popularity) {
+   this.title = title;
+   this.overview = overview;
+   this.releaseDate = releaseDate;
+   this.popularity = popularity;
+ }
+}
+ 
+function makeMoviesArray(city) {
+ const moviesArr = city.map(el => {
+   let title = el.title;
+   let overview = el.overview;
+   let releaseDate = el.release_date;
+   let popularity = el.popularity;
+   return new Movie(title, overview, releaseDate, popularity);
+ });
+ return moviesArr;
+}
+ 
+ 
+app.get('/', (request, response) => {
+ response.send('hello from home!!');
 });
-
-// error handling//
-
-server.use('*', (error, request, response, next) => {
-  response.status(500).send(error);
+ 
+app.get('/weather', (request, response) => {
+ 
+ let url = `https://api.weatherbit.io/v2.0/forecast/daily?&key=582f15fe04aa420bae9a3cf75952050fc&lat=${request.query.lat}&lon=${request.query.lon}`;
+ 
+ axios.get(url)
+   .then(res => {
+     // console.log('res.data.data', res.data.data);
+     let forecastArr = makeForecastArray(res.data.data);
+     response.send(forecastArr);
+   })
+   .catch((e) => {
+     console.log(e);
+     response.status(500).send(e);
+   });
 });
-
-server.use('*', (request, response) => {
-  response.status(404).send('Route not found');
+ 
+app.get('/movies', (request, response) => {
+ 
+ let url = `https://api.themoviedb.org/3/movie/550?api_key=73e778dc212937332b7eca65d749549a&query=${request.query.cityName}`;
+ // console.log('movie url',url);
+ 
+ axios.get(url)
+   .then(res => {
+     console.log('res.data', res.data);
+     let moviesArr = makeMoviesArray(res.data.results);
+     response.send(moviesArr);
+   })
+   .catch((e) => {
+     console.log(e);
+     response.status(500).send(e);
+   });
 });
-
-//server opens up to  listen//
-
-server.listen(PORT, () => {
-  console.log('Server is running on port : ' + PORT);
+ 
+app.use('*', (error, request, response, next) => {
+ response.send(500).send(error);
 });
-
-
+ 
+// opens up the server for requests
+app.listen(PORT, () => {
+ console.log('Server is running on port :: ' + PORT);
+});
 
