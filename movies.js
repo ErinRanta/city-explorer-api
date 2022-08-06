1eedc847090f3ee9c7457b9cbca465a0d63bb56c
Erin Ranta <erinran99@gmail.com>
2022-08-06 18:27:16 -0400

    search

--- movies.js
+++ movies.js
@@ -1,21 +1,30 @@
-class Movie {
-    constructor(title, overview, releaseDate, popularity) {
-      this.title = title;
-      this.overview = overview;
-      this.releaseDate = releaseDate;
-      this.popularity = popularity;
-    }
+const axios = require('axios').default;
+
+
+class Movies {
+  constructor(movie) {
+    this.title = movie.title;
+    this.overview = movie.overview;
+    this.average_votes = movie.vote_average;
+    this.total_votes = movie.vote_count;
+    this.image_url = `https://image.tmdb.org/t/p/w300/${movie.poster_path}`;
+    this.popularity = movie.popularity;
+    this.released_date = movie.release_date;
   }
-  
-  const makeMoviesArray = function(city) {
-    const moviesArr = city.map(el => {
-      let title = el.title;
-      let overview = el.overview;
-      let releaseDate = el.release_date;
-      let popularity = el.popularity;
-      return new Movie(title, overview, releaseDate, popularity);
+}
+async function handleMovies(request, response) {
+  let search = request.query.query;
+  let movieKey = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${search}`;
+  await axios.get(movieKey)
+    .then(res => {
+      console.log('this is supposed to be the query:', request.query.search);
+      console.log(res.data.results.map(movie => new Movies(movie)));
+      response.send(res.data.results.map(movie => new Movies(movie)));
+    })
+    .catch((e) => {
+      console.log(e);
+      response.status(500).send(e);
     });
-    return moviesArr;
-  };
-  
-  module.exports = makeMoviesArray;
+}
+
+module.exports = handleMovies;
