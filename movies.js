81f1e86a2890fd9b50344477232ec463b6b0c706
Erin Ranta <erinran99@gmail.com>
2022-08-09 16:36:57 -0400

    modules

--- movies.js
+++ movies.js
@@ -1,14 +1,49 @@
 
 
-// const axios = require('axios').default;
-// const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
+'use strict';
 
-// class Movies {
-//     constructor (title, summary, genre, ......more....)
-//     this.title = title;
-//     this.summary = summary;
-//     this.genre = genre;
+require('dotenv').config();
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const movieAPIKey = process.env.REACT_APP_TMDB_API_KEY;
+app.use(cors());
+
+class Movie {
+  constructor(title, overview, releaseDate, popularity) {
+    this.title = title;
+    this.overview = overview;
+    this.releaseDate = releaseDate;
+    this.popularity = popularity;
+  }
+}
+
+const getMovies = (cityName, response) => {
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${movieAPIKey}&query=${cityName}`;
+  axios.get(url)
+    .then(res => {
+      let moviesArr = makeMoviesArray(res.data.results);
+      response.send(moviesArr);
+    })
 
 
+    .catch((e) => {
+      console.log('movies error',e);
+      response.status(500).send(e);
+    });
+};
 
-// }
+const makeMoviesArray = (city) => {
+  const moviesArr = city.map(el => {
+    let title = el.title;
+    let overview = el.overview;
+    let releaseDate = el.release_date;
+    let popularity = el.popularity;
+    return new Movie(title, overview, releaseDate, popularity);
+  });
+  return moviesArr;
+};
+
+module.exports = getMovies;
--- server.js
+++ server.js
@@ -1,110 +1,34 @@
 'use strict';
- 
+
 require('dotenv').config();
-const axios = require('axios');
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
-const PORT = process.env.PORT;
- 
- 
+const cors = require('cors');
 app.use(cors());
- 
+const PORT = process.env.PORT;
+const getWeather = require('./weather');
+const getMovies = require('./movies');
 
 
-
-
-class Forecast {
- constructor(date, condition, high, low) {
-   this.date = date;
-   this.condition = condition;
-   this.high = high;
-   this.low = low;
-   this.weatherAPIKey = process.env.REACT_APP_WEATHERBIT_API_KEY;
-   this.movieAPIKey = process.env.REACT_APP_TMDB_API_KEY;
- }
-}
- 
-function makeForecastArray(weatherCity) {
- const forecastArr = weatherCity.map(el => {
-   let date = el.valid_date;
-   let condition = el.weather.description;
-   let high = el.high_temp;
-   let low = el.low_temp;
-   return new Forecast(date, condition, high, low);
- });
- return forecastArr;
-}
- 
-class Movie {
- constructor(title, overview, releaseDate, popularity) {
-   this.title = title;
-   this.overview = overview;
-   this.releaseDate = releaseDate;
-   this.popularity = popularity;
- }
-}
- 
-function makeMoviesArray(city) {
- const moviesArr = city.map(el => {
-   let title = el.title;
-   let overview = el.overview;
-   let releaseDate = el.release_date;
-   let popularity = el.popularity;
-   return new Movie(title, overview, releaseDate, popularity);
- });
- return moviesArr;
-}
- 
- 
 app.get('/', (request, response) => {
- response.send('hello from home!!');
+  response.send('hello!');
 });
- 
+
 app.get('/weather', (request, response) => {
- 
- let url = `https://api.weatherbit.io/v2.0/forecast/daily?&key=key=${key}&city=${searchQuery}&days=5`;
- 
- axios.get(url)
-   .then(res => {
-     // console.log('res.data.data', res.data.data);
-     let forecastArr = makeForecastArray(res.data.data);
-     response.send(forecastArr);
-   })
-   .catch((e) => {
-     console.log(e);
-     response.status(500).send(e);
-   });
+  getWeather(request.query.lat, request.query.lon, response);
 });
- 
+
 app.get('/movies', (request, response) => {
- 
- let url = `https://api.themoviedb.org/3/movie?api_key=${process.env.MOVIE_API_KEY}&query=${search}`;
- // console.log('movie url',url);
- 
- axios.get(url)
-   .then(res => {
-     console.log('res.data', res.data);
-     let moviesArr = makeMoviesArray(res.data.results);
-     response.send(moviesArr);
-   })
-   .catch((e) => {
-     console.log(e);
-     response.status(500).send(e);
-   });
+  getMovies(request.query.cityName, response);
 });
 
- 
-app.use('*', (error, request, response, next) => {
- response.send(500).send(error);
+app.use('*', (error, request, response) => {
+  response.send(500).send(error);
 });
- 
-// opens up the server for requests
+
 app.listen(PORT, () => {
- console.log('Server is running on port :: ' + PORT);
+  console.log('Server is running on port :: ' + PORT);
 });
-
 
 
 
