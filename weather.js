8c35ec0ae4ec8e2f0aba25456e260e2ab6b38e36
Erin Ranta <erinran99@gmail.com>
2022-08-09 16:43:03 -0400

    module2

--- weather.js
+++ weather.js
@@ -1,74 +1,52 @@
-// import { Component } from 'react';
-// import axios from 'axios';
+'use strict';
 
-// class Weather extends Component {
-//   constructor(){
-//     super();
-//     this.state = {
-//       lat: 0,
-//       lon: 0,
-//       searchQuery: '',
-//       weather: [],
-//       error: {},
-//     }
-//   }
+require('dotenv').config();
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 
-//   handleSubmit = (e) => {
-//     e.preventDefault();
-//     this.getLocation();
-//   }
+const weatherAPIKey = process.env.REACT_APP_WEATHERBIT_API_KEY;
+app.use(cors());
 
-//   // send request to get lat lon
-//   getLocation = async () => {
-//     let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.searchQuery}&format=json`;
+class Forecast {
+  constructor(date, condition, high, low) {
+    this.date = date;
+    this.condition = condition;
+    this.high = high;
+    this.low = low;
+  }
+}
 
-//     try {
+const getWeather = (lat, lon, response) => {
 
-//       let response = await axios.get(url);
-//       this.setState({
-//         lat: response.data[0].lat,
-//         lon: response.data[0].lon,
-//       }, this.getWeather); // set state can take an optional call back to run, AFTER state is set.
+  
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${weatherAPIKey}&lat=${lat}&lon=${lon}&units=I`;
 
-//     } catch (e) {
-//       this.setState({ error: e });
-//     }
-//   }
+  axios.get(url)
 
-//   // send request to get weather values
-//   getWeather = async () => {
-//     let url = `http://localhost:3001/weather?lat=${this.state.lat}&lon=${this.state.lon}&searchQuery=${this.state.searchQuery}`;
+    .then(weatherGet => {
+      let forecastArr = makeForecastArray(weatherGet.data.data);
+      console.log('weather.js .then forecastArr[0]', forecastArr[0]);
+      response.send(forecastArr);
+    })
+    .catch((e) => {
+      console.log('error', e);
+      response.status(500).send(e);
+    });
+};
 
-//     try {
+const makeForecastArray = (arr) => {
+  console.log('makeForecastArray arr[0]',arr[0]);
+  const forecastArr = arr.map(el => {
+    let date = el.valid_date;
+    let condition = el.weather.description;
+    let high = el.high_temp;
+    let low = el.low_temp;
+    return new Forecast(date, condition, high, low);
+  });
+  console.log('makeForecastArr forecastArr[0]',forecastArr[0]);
+  return forecastArr;
+};
 
-//       let response = await axios.get(url);
-//       this.setState({
-//         weather: response.data,
-//       });
-
-//     } catch (e) {
-//       this.setState({ error: e });
-//     }
-
-//   }
-
-//   render() {
-//     console.log(this.state);
-//     return(
-//       <>
-//       <h1>Weather Data!</h1>
-//         <form onSubmit={this.handleSubmit}>
-//           <input onChange={(e) => this.setState({ searchQuery: e.target.value })}/>
-//           <button>Explore!</button>
-//         </form>
-//         <div>
-//           <h2>{this.state.searchQuery}</h2>
-//           <p>{this.state.lat}</p>
-//           <p>{this.state.lon}</p>
-//         </div>
-//       </>
-//     )
-//   }
-// }
-
-// export default weather;
+module.exports = getWeather;
